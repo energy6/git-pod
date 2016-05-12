6d8f3b96768ccb51e6ee5144d60a3577afb21551
Vassilis Rizopoulos <vassilisrizopoulos@gmail.com>
2016-05-12 11:32:29 +0200

    fix the ordering of before/afters and create a test git repo on setup

--- test/test_create.rb
+++ test/test_create.rb
@@ -1,35 +1,27 @@
 require 'tmpdir'
+require 'fileutils'
 require 'minitest/autorun'
-require 'git-pod'
+require_relative '../lib/git-pod'
 
 describe GitPod::Create do
   before do
     @tempdir = Dir.mktmpdir()
-    p @tempdir
-    Git.init(@tempdir)
+    repo=Git.init(@tempdir)
+    FileUtils.touch "#{@tempdir}/README.txt"
+    repo.add "#{@tempdir}/README.txt"
+    repo.commit "Initial"
   end
 
   after do
-    puts @tempdir
-    # Dir.rmdir(@tempdir)
+    FileUtils.rm_rf(@tempdir)
   end
 
   describe "creating a new pod" do
-    Dir.chdir(@tempdir) do
-      command = GitPod::Create.new ["-d A unit test created pod.", "testPod"]
-
-      it "leads to a new pod master branch" do
+    it "leads to a new pod master branch" do
+      Dir.chdir(@tempdir) do
+        command = GitPod::Create.new ["-d A unit test created pod.", "testPod"]
         command.exec()
       end
-
     end
   end
-end
-
-class CreateTest < Minitest::Unit::TestCase
-
-  def test_easy
-    puts "Yeah!"
-  end
-
 end
